fb7d9903bb1614f221485343727bf40005d0a2cd
Pablo <carrazanap@gmail.com>
2026-04-07 20:43:00 -0300

    Update login

--- backend/src/obj/Debug/net8.0/PracticaProfesional.AssemblyInfo.cs
+++ backend/src/obj/Debug/net8.0/PracticaProfesional.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PracticaProfesional")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70efc0696bccfbbdea2a74c1971cce3ada56e648")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05e198ca31ecaf0431c67d4a1675c5d2845c9720")]
 [assembly: System.Reflection.AssemblyProductAttribute("PracticaProfesional")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PracticaProfesional")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
