0aa556b151090398a25258feb1ecf6cf6ce2d6ea
Pablo <carrazanap@gmail.com>
2026-05-11 12:54:32 -0300

    Deploy Azure Backend Frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://PLACEHOLDER-APP-SERVICE.azurewebsites.net/api'
+  apiUrl: 'https://escmb-practicaprof.azurewebsites.net/api'
 };
